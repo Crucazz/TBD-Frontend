c760f93a0ad9d7c29453db273d9ae68b51d817e1
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2020-05-27 18:22:49 -0400

    Layout.vue actualizado con Voluntarios

--- front-tbd/src/components/Layout.vue
+++ front-tbd/src/components/Layout.vue
@@ -25,6 +25,16 @@
             </v-list-item-content>
           </v-list-item>    
         </router-link>
+        <router-link to="/api/volunteer">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-contact-mail</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Voluntarios</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>    
+        </router-link>
       </v-list>
     </v-navigation-drawer>
 
