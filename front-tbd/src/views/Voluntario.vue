29cbd5a3803cfaa878862528bf624f074f2eda89
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2020-05-27 17:57:44 -0400

    Voluntario.vue actualizado

---
+++ front-tbd/src/views/Voluntario.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="5"
+    item-key="name"
+    class="elevation-1"
+    :footer-props="{
+      showFirstLastPage: true,
+      firstIcon: 'mdi-arrow-collapse-left',
+      lastIcon: 'mdi-arrow-collapse-right',
+      prevIcon: 'mdi-minus',
+      nextIcon: 'mdi-plus'
+    }"
+  ></v-data-table>
+
+</template>
+
+
+
+
+<script>
+  export default {  
+
+    data(){
+        return{
+            //Lista de ítems a mostrar
+            items:[],
+            headers: [
+          {
+            text: 'ID',
+            align: 'start',
+            value: 'id',
+          },
+          { text: 'Nombre', value: 'name' },
+        ],
+        }
+    },
+    methods:{
+        //Función asíncrona para consultar los datos
+        getData: async function(){
+            try {
+                let response = await this.$http.get('/api/v1/volunteer');
+                this.items  = response.data;
+                
+            } catch (error) {
+                console.log('error', error);
+            }
+        },
+
+        
+
+    },
+    //Función que se ejecuta al cargar el componente
+    created:function(){
+        this.getData();
+    }
+
+  }
+</script>
